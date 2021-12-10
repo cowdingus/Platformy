ce5fbfd4aa14f3facd95d3a69af47c19ce73371a
cowdingus <zaki.nadhif45@gmail.com>
2021-12-10 17:23:10 +0700

    Make Grid2D throws out_of_range on invalid coords

--- header/Data/Grid2D.hpp
+++ header/Data/Grid2D.hpp
@@ -2,6 +2,7 @@
 
 #include "json.hpp"
 
+#include <stdexcept>
 #include <vector>
 #include <cassert>
 
@@ -23,8 +24,8 @@ public:
     size_t getHeight() const;
 
 private:
-    size_t width {};
-    size_t height {};
+    size_t m_width {};
+    size_t m_height {};
 
     size_t toIndex(size_t x, size_t y) const;
     std::vector<T> m_data;
@@ -38,14 +39,14 @@ private:
 
 template <typename T>
 inline Grid2D<T>::Grid2D(size_t width, size_t height, T fill)
-    : width(width), height(height), m_data(width * height, fill)
+    : m_width(width), m_height(height), m_data(width * height, fill)
 {
 
 }
 
 template <typename T>
 inline Grid2D<T>::Grid2D(size_t width, size_t height, const std::vector<T>& fill)
-    : width(width), height(height), m_data(fill)
+    : m_width(width), m_height(height), m_data(fill)
 {
     assert(m_data.size() == width * height);
 }
@@ -53,46 +54,49 @@ inline Grid2D<T>::Grid2D(size_t width, size_t height, const std::vector<T>& fill
 template <typename T>
 inline void Grid2D<T>::set(size_t x, size_t y, T value)
 {
+    if (x > m_width || y > m_height) throw std::out_of_range("Given coordinates exceed bounds");
     m_data.at(toIndex(x, y)) = value;
 }
 
 template <typename T>
 inline T& Grid2D<T>::get(size_t x, size_t y)
 {
+    if (x > m_width || y > m_height) throw std::out_of_range("Given coordinates exceed bounds");
     return m_data.at(toIndex(x, y));
 }
 
 template <typename T>
 inline const T& Grid2D<T>::get(size_t x, size_t y) const
 {
+    if (x > m_width || y > m_height) throw std::out_of_range("Given coordinates exceed bounds");
     return m_data.at(toIndex(x, y));
 }
 
 template <typename T>
 inline size_t Grid2D<T>::getWidth() const
 {
-    return width;
+    return m_width;
 }
 
 template <typename T>
 inline size_t Grid2D<T>::getHeight() const
 {
-    return height;
+    return m_height;
 }
 
 template <typename T>
 inline size_t Grid2D<T>::toIndex(size_t x, size_t y) const
 {
-    assert(x < width && y < height);
-    return y * width + x;
+    assert(x < m_width && y < m_height);
+    return y * m_width + x;
 }
 
 template <typename T_>
 inline void to_json(json& j, const Grid2D<T_>& g)
 {
     j = json{
-        {"width", g.width},
-        {"height", g.height},
+        {"width", g.m_width},
+        {"height", g.m_height},
         {"data", g.m_data}
     };
 }
@@ -100,7 +104,7 @@ inline void to_json(json& j, const Grid2D<T_>& g)
 template <typename T_>
 inline void from_json(const json& j, Grid2D<T_>& g)
 {
-    j.at("width").get_to(g.width);
-    j.at("height").get_to(g.height);
+    j.at("width").get_to(g.m_width);
+    j.at("height").get_to(g.m_height);
     j.at("data").get_to(g.m_data);
 }
